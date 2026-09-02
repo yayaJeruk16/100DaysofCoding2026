20796fe9814900b39595cbb12cd539865fd2578b
yayaJeruk16 <rreassy@gmail.com>
2026-09-02 19:58:49 +0800

    Create Day1.java

---
+++ Day1.java
@@ -0,0 +1,15 @@
+public class ContohKomentar {
+    // komentar apa aja kayak menjalankan program atau kode
+    
+    /* nah kalau ini yang banyak
+     * kayak bisa buat banyak
+     * baris sekaligus loh ya
+     */ 
+     
+     // contohnya komentar garis
+    // nah ini method main kayak titik awal program berjalan
+    public static void main(String[] args) {
+        System.out.println("hai day one");
+        
+        }
+    }
