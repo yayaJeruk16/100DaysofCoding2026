27e6011b5888ccbe2b3031df27042eed5d1d0a63
yayaJeruk16 <rreassy@gmail.com>
2026-09-03 16:24:41 +0800

    Update Day2.java

--- Day2.java
+++ Day2.java
@@ -3,10 +3,9 @@ public class day2 {
         String nama = "Yaya";
         int umur = 15;
         double tinggiBadan = 150.0;
-        
-        System.out.printf("nama aku: %s%n", nama);
-        System.out.print("umur aku:" + umur + "\n");
-        System.out.println("tinggi badan aku : " + tinggiBadan + "\n" );
+        System.out.printf("nama aku: %s%n", nama); // fungsinya jalanin teks pake format,biasanya cocok buat tampilkan angka dan variabel
+        System.out.print("umur aku:" + umur + "\n"); // fungsinya buat jalanin teks tapi dinaris yang sama,tidak bisa ganti baris 
+        System.out.println("tinggi badan aku : " + tinggiBadan + "\n" ); //yang ini fungsinya jalanin teks dan bisa langsung buat baris baru
     }
     
 }
