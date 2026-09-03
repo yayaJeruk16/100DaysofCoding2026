132b7bf9fb57cd9815b98f2a0a7ab67fe428dfdb
yayaJeruk16 <rreassy@gmail.com>
2026-09-03 15:53:40 +0800

    Create Day2.java
    Nah untuk penjelasannya itu
    
    Print =  jalanin teks tapi di baris yang sama kak dan tidak bisa ganti baris
    Println = nah ini samaji jalanin teks cuman dia bisa langsung pindah ke baris baru setelahnya.
    Printf = jalanin teks pake format, biasanya cocok buat tampilkan angka atau variabel

---
+++ Day2.java
@@ -0,0 +1,12 @@
+public class day2 {
+    public static void main(String[] args){
+        String nama = "Yaya";
+        int umur = 15;
+        double tinggiBadan = 150.0;
+        
+        System.out.printf("nama aku: %s%n", nama);
+        System.out.print("umur aku:" + umur + "\n");
+        System.out.println("tinggi badan aku : " + tinggiBadan + "\n" );
+    }
+    
+}
