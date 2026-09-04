fb2fe5470e1c1c0fb0faf2823f43157e934e8ebc
yayaJeruk16 <rreassy@gmail.com>
2026-09-04 14:44:50 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,16 @@
+public class day3 {
+    public static void main(String[] args){
+        System.out.println("yaya: \"jam berapa deadline\""); // \" ini untuk menampilkan tanda kutip dua
+        System.out.println("kak fiki: \"23.59 yaya\"");
+        System.out.printf("yaya manis\n");// \n ini digunakan untuk buat baris baru
+        System.out.println("haii\b ini dunia yaya"); // \b ini untuk  menghapus karakter sebelah kiri
+        System.out.println("kalau bicara tentang \'matahari\' nah itu aku"); // \' kalau ini untuk menampilkan tanda kutip satu
+        System.out.println("total coding yaya masih 3\f"); // \f untuk menampilkan halaman (walau jarang digunakan)
+        System.out.print("waktu\r sisa:" +1+ "jam" +"\n"); // \r untuk menarik kursor ke paling awal
+        System.out.println("  /\\");
+        System.out.println(" /  \\"); // ini untuk menampilkan garis miring
+        System.out.println("/____\\");
+        System.out.print("\t yiayy yaya hebat");// \t ini untuk menambahkan spasi seperti tab di keyboard yang spasinya panjang
+    }
+    
+}
