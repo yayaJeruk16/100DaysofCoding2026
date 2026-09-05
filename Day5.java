6c13de478f38eb343d272fd772ee51f00e8cd668
yayaJeruk16 <rreassy@gmail.com>
2026-09-06 00:26:30 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,11 @@
+public class Day5 {
+    public static void main(String[] args){
+        System.out.println("\tTipe data pecahan \'Double dan Float\'\n");
+        double nilai = 95.555555555555; //biasanya ini digunakan untuk perhitungan yang akurat
+        float ipk = 3.8777777f; //kalau ini digunakan untuk data yang banyak biasanya dan bisa juga mengukur suhu
+        
+        System.out.println("Nilai rata-rata Yaya mencapai\t: " + nilai);
+        System.out.println("Dengan itu Yaya lulus dengan ipk: " + ipk);
+    }
+    
+}
