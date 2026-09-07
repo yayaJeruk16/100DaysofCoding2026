d44e00a19963960987559f6f6dfdb52f1a483675
yayaJeruk16 <rreassy@gmail.com>
2026-09-07 11:38:39 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,23 @@
+public class Day6 {
+    public static void main(String[] args){
+        //TIPE DATA KARAKTER(CHAR) DIGUNAKAN UNTUK MENYIMPAN 1 KARAKTER SAJA
+        char inisial = 'L';
+        char simbol = '$';
+        char nilai = 'A';
+        //char nilai = 'AB'; //ini salah kak, karena char cuma boleh 1 karakter
+        
+        System.out.println("Inisal kucing Yaya: " + inisial);
+        System.out.println("Nilai Yaya: " + nilai);
+        
+        //TIPE DATA BOOLEAN YANG DIGUNAKAN UNTUK LOGIKA DAN HANYA MEMILIKI 2 NILAI (TRUE DAN FALSE)
+        boolean Cantik = true;
+        boolean Jelek = false;
+        //boolean cantik = True; //ini salah kak karena T besar dan itu bukan kata kunci java
+        
+        System.out.println("Yaya Cantik: " + Cantik);
+        System.out.println("Yaya Jelek: " + Jelek);
+        
+        
+    }
+    
+}
