a4aef7d02cd942d0732411a2fef74b1f1d079d47
yayaJeruk16 <rreassy@gmail.com>
2026-09-08 09:32:15 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,17 @@
+public class Day7 {
+    public static void main(String[] args){
+        //PENGENALAN TIPE DATA STRING UNTUK TEKS.
+        String favFruit = "Yaya suka sekali sama jeruk";
+        String fav_Song = "Yes, Yaya suka taylor karena bawaannya happy aja gitu";
+        String favFood = "Yaya suka roti juga";
+        String end = "Yah begitulah Yaya";
+        
+        System.out.println(favFruit);
+        System.out.println(fav_Song);
+        System.out.println(favFood);
+        System.out.println(end);
+        
+        //tidak pake kutip di sout karna langsunh memanggil variabel biar Yaya tidak tulis ulang kak hehe..
+    }
+    
+}
