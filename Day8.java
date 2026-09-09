021b709a7efaf40dfdaf0820fda3ea0d8ea3a482
yayaJeruk16 <rreassy@gmail.com>
2026-09-09 13:28:23 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,9 @@
+public class Day8 {
+    public static void main(String[] args){
+        //DEKLARASI, INISIALISASI, DAN UPDATE VARIABEL
+        char simbol = '#';
+        //INISIALISASI = '#'
+        //UPDATE VARIABEL SEPERTI
+        simbol = '$';
+    }
+}
