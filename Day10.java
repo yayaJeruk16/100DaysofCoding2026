b85e145dc684132e94107a0a9fc31159cc99d163
yayaJeruk16 <rreassy@gmail.com>
2026-09-11 00:01:29 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,18 @@
+public class Day10 {
+    public static void main(String[] args) {
+	//PEMAHAMAN BATAS MINIMAL DAN MAKSIMAL DARI TIPE DATA
+    	System.out.println("byte   : " + Byte.MIN_VALUE + " sampai " + Byte.MAX_VALUE);
+    	System.out.println("long   : " + Long.MIN_VALUE + " sampai " + Long.MAX_VALUE);
+    	System.out.println("short  : " + Short.MIN_VALUE + " sampai " + Short.MAX_VALUE);
+    	System.out.println("int    : " + Integer.MIN_VALUE + " sampai " + Integer.MAX_VALUE);
+    	System.out.println("double : " + Double.MIN_VALUE + " sampai " + Double.MAX_VALUE);
+    	System.out.println("float  : " + Float.MIN_VALUE + " sampai " + Float.MAX_VALUE);
+    	System.out.println("char   : " + (int)Character.MIN_VALUE + " sampai " + (int)Character.MAX_VALUE);
+    	
+    	//KENAPA PAKE INT, AGAR YANG KELUAR BILANGAN BULAT BUKAN KARAKTER
+    	
+    	//boolean = tidak ada batas juga karena dia cuma dua kondisi.
+    	
+}
+    
+}
