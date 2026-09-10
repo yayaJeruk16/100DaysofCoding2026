5d1b21dd8ebe225105358166113b6d406698dd3a
yayaJeruk16 <rreassy@gmail.com>
2026-09-10 09:14:54 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,8 @@
+public class Day9 {
+    public static void main(String[] args) {
+	//COSTANTA (FINAL VARIABEL)
+        final int umur = 29;
+        //umur = 25; (TIDAK BISA DI UBAH, KARENA COSTANTA ADALAH NILAI TETAP.
+}
+    
+}
