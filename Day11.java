9797adf17d27cc07735ff70dd53635375510edb3
yayaJeruk16 <rreassy@gmail.com>
2026-09-12 00:01:18 +0800

    Create Day11.java

---
+++ Day11.java
@@ -0,0 +1,12 @@
+import java.util.Scanner; //panggil Scanner nyaa nieh
+
+public class Day011 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in); //nah ini sa buat objek nya Scanner
+        double tb = sc.nextDouble(); //nah kalau ini dia baca angka yang pake koma bukan si?
+        
+        System.out.println(tb); //udah deh
+    }
+    
+    
+}
