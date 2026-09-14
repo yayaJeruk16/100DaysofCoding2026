3e9f49a3b1492ab99a519d99739d6a6f0569a332
yayaJeruk16 <rreassy@gmail.com>
2026-09-14 20:36:11 +0800

    Create Day13.java

---
+++ Day13.java
@@ -0,0 +1,16 @@
+public class Day13 {
+    public static void main(String[] args){
+        
+    //PROGRAM YANG MENDEKLARASIKAN DAN MENAMPILKAN DATA BERIKUT MENGGUNAKAN TIPE DATA YANG PALING SESUAI.
+    byte saudara = 2;
+    long penduduk = 5000000L;
+    float tb = 1.65f;
+    double bb = 58.75;
+    
+        System.out.println("Jumlah Saudara: " + saudara);
+        System.out.println("Jumlah Penduduk: " + penduduk);
+        System.out.println("Tinggi Badan: " + tb + " meter");
+        System.out.println("Berat badan: " + bb + " kg");
+        
+}
+}
