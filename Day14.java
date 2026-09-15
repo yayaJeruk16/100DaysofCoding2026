129aa100a99e8f078a3ee16ee5a94ed83667aa8d
yayaJeruk16 <rreassy@gmail.com>
2026-09-15 19:53:47 +0800

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,14 @@
+public class main {
+    public static void main(String[] args) {
+      int a = 10;
+      int b = 5;
+      
+      int c = a + b;
+      
+      System.out.println("nilai a tambah b = " + c);
+      c = a - b;
+      
+      
+      System.out.println("nilai a kurang b = " + c);
+    }
+}
