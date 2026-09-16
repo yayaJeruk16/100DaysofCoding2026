e169d38ec38853d49871aa4409b68cdd8257778c
yayaJeruk16 <rreassy@gmail.com>
2026-09-16 12:14:05 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,13 @@
+public class Day15 {
+    public static void main(String[] args){
+        int a = 15;
+        int b = 5;
+        int c = a * b;
+        
+        System.out.println("Nilai a dikali b = " + c);
+        
+        c = a/b;
+        
+        System.out.println("Nilai a dibagi b = " + c);
+    }
+}
