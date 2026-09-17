f741fcec0b137fbd4852f6ba93748c99cc8dede3
yayaJeruk16 <rreassy@gmail.com>
2026-09-17 14:21:04 +0800

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class LatihanDay16 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan jam saat ini (0-23): ");
+        int jam = input.nextInt();
+        System.out.print("Berapa jam lagi dimulai?: ");
+        int jamMulai = input.nextInt();
+        
+        int acaraMulai = jam % 24 + jamMulai;
+        System.out.println("Acara akan dimulai pada jam: " + acaraMulai);
+    }
+    
+}
