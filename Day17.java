8b4a4e67e47cd833331ebc93e6f3c01bff9c021c
yayaJeruk16 <rreassy@gmail.com>
2026-09-18 18:44:34 +0800

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,27 @@
+public class Day17 {
+    public static void main(String[] args){
+        //PENUGASAN = , =+ , =- , =* , =/, =%
+        double a = 32.6 ;
+        a += 1;
+        System.out.println(a);
+        
+        int b = 10;
+        b -= 5;
+        System.out.println(b);
+        
+        byte c = 2;
+        c *= 4;
+        System.out.println(c);
+        
+        long d = 124;
+        d /= 50;
+        System.out.println(d);
+        
+        short e = 90;
+        e %= 7;
+        System.out.println(e);
+        
+        
+    }
+    
+}
