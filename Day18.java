be578472e1917be5644a4104a222aa2f6d927e84
yayaJeruk16 <rreassy@gmail.com>
2026-09-19 16:21:35 +0800

    Update Day18.java

--- Day18.java
+++ Day18.java
@@ -3,11 +3,5 @@ public class Day18 {
         byte a = 6;
         int b = a; //ini otomatis bisa yaa jadi aman
         System.out.println(b);
-        
-        int c = 389;
-        byte d = (byte) + c; //kalau ini namanya konversi paksa
-        System.out.println(d);
-        
-        
     }
 }
