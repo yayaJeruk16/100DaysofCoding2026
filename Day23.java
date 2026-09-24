c540345eaf821f1a3411bb018d6e2ddbdd71926c
yayaJeruk16 <rreassy@gmail.com>
2026-09-24 12:45:59 +0800

    Create Day23.java

---
+++ Day23.java
@@ -0,0 +1,8 @@
+public class Day23 {
+    public static void main(String[] args){
+        double SisiPersegi = 12.5;
+        double luas = SisiPersegi*SisiPersegi;
+        
+        System.out.println("Luas persegi = " + luas + " cm²");
+    }
+}
